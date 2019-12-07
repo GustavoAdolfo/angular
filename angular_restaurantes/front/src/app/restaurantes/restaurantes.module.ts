d6b252d8814f091e3b9f7eb5752c9a04ce578fcf
Gustavo Adolfo <gustavo.adolfo.as@gmail.com>
2019-12-07 16:14:38 -0300

    commit inicial

---
+++ angular_restaurantes/front/src/app/restaurantes/restaurantes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestaurantesComponent } from './restaurantes.component';
+
+@NgModule({
+  declarations: [RestaurantesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RestaurantesModule { }
